37bb7f1c5c6b6f944ff04f583946ae57596c692b
nharringtonwasatch <nharrington@wasatchphotonics.com>
2015-09-15 16:45:57 -0400

    basic result objects

--- barbecue/model.py
+++ barbecue/model.py
@@ -3,6 +3,8 @@
 
 import logging
 
+from wasatchcameralink import simulation
+
 log = logging.getLogger(__name__)
 
 class Model(object):
@@ -14,3 +16,40 @@ class Model(object):
         log.info("Model created")
         self.results = []
         self.device = None
+
+    def assign(self, device_type):
+        """ Designate and setup the specified device type
+        """
+        if device_type == None:
+            raise(ValueError, "specify a device type")
+
+        if device_type != "simulation":
+            raise(ValueError, "specify a valid device type")
+        
+        self.device = device_type
+        return True
+
+    def scan(self, gain, offset):
+        if self.device == None:
+            raise(ValueError, "Must assign device first")
+
+        if gain == None or offset == None:
+            raise(ValueError, "must specify gain, offset")
+
+        self.device = simulation.SimulatedSpectraDevice()
+        result = self.device.setup_pipe()
+        log.info("Setup result is: %s" % result)
+
+        result, data = self.device.grab_pipe()
+        store_result = Result(gain, offset, data)
+        self.results.append(store_result)
+        return True
+            
+class Result(object):
+    """ holds stored data and device settings from a given scan.
+    """
+    def __init__(self, gain=-1, offset=-1, data=[]):
+        super(Result, self).__init__()
+        self.gain = gain
+        self.offset = offset
+        self.data = data
--- barbecue/test/test_model.py
+++ barbecue/test/test_model.py
@@ -16,6 +16,45 @@ class Test(unittest.TestCase):
         self.assertEqual(len(self.model.results), 0)
         self.assertIsNone(self.model.device)
 
+    def test_assignment(self):
+        # Attempt to assign an invalid device, catch exception
+        self.assertRaises(ValueError, self.model.assign, None)
+        self.assertRaises(ValueError, self.model.assign, "KnownInvalid")
+        
+        # Pass with the simulation assignment
+        self.assertTrue(self.model.assign("simulation"))
+
+    def test_result(self):
+        # result object has expected default values
+        res = model.Result()
+        self.assertEqual(res.gain, -1)
+        self.assertEqual(res.offset, -1)
+        self.assertEqual(len(res.data), 0)
+        
+
+    def test_scan_result(self):
+
+        scan = self.model.scan
+
+        # Attempt to scan before device assignment, fail
+        self.assertRaises(ValueError, scan, gain=None, offset=None)
+
+        self.model.assign("simulation")
+
+        # Attempt to scan with no gain, offset, fail
+        self.assertRaises(ValueError, scan, gain=None, offset=None)
+
+        # Run a succesful scan, make sure the results list is populated
+        result = scan(gain=1, offset=10)
+        self.assertEqual(len(self.model.results), 1)
+
+        # Make sure the results list data items match expected values
+        last_result = self.model.results[-1]
+        print "Result is: [%s]" % last_result
+        self.assertEqual(last_result.gain, 1)
+        self.assertEqual(last_result.offset, 10)
+        self.assertEqual(len(last_result.data), 2048)
+
 if __name__ == "__main__":
     unittest.main()
     
